4514bb9dea26d61928408dd5d7efd0e0fb90301d
Rupa Dey <68509719+Rupa-Dey@users.noreply.github.com>
2024-01-05 18:54:20 +0600

    Update Housing.java

--- HousingStructure/HousingStructure/Housing.java
+++ HousingStructure/HousingStructure/Housing.java
@@ -1,37 +1,41 @@
 package HousingStructure;
 
+//package CompositeDesignPattern;
+
 import java.util.ArrayList;
 
-public class Housing implements IStructure{
-    private ArrayList< IStructure > structures;
+public class Housing implements IStructure {
     private String address;
-    public Housing(String address) {
-        this.structures = new ArrayList < IStructure > ();
+    private ArrayList<IStructure> structures;
+
+    public Housing(String address){
+        this.structures = new ArrayList<IStructure>();
         this.address = address;
     }
-    public String getName() {
-        return this.address;
+    public String getAddress(){
+        return  this.address;
     }
-    public int addStructure(IStructure component) {
-        this.structures.add(component);
-        return this.structures.size() - 1;
-    }
-    public IStructure getStructure(int componentNumber) {
-        return this.structures.get(componentNumber);
-    }
-    public void location() {
-        System.out.println("You are currently in " +
-                this.getName() + ". It has ");
-        for (IStructure struct: this.structures)
-            System.out.println(struct.getName());
-    }
-    public void enter() {
-        System.out.println("You have entered the " +
-                this.getName());
-    }
-    public void exit() {
-        System.out.println("You have left the " +
-                this.getName());
-    }
-}
 
+    public int addStructure(IStructure component){
+        this.structures.add(component);
+        return this.structures.size()-1;
+
+    }
+    public IStructure getStructure(int componentNum){
+        return this.structures.get(componentNum);
+    }
+    public void location(){
+        System.out.println("You are currently in the " + this.getAddress() +". but it has ");
+        for (IStructure iStructure: this.structures){
+            System.out.println(iStructure.getAddress());
+        }
+
+    }
+    public void enter(){
+        System.out.println("You have entered the " +this.getAddress());
+    }
+    public void exit(){
+        System.out.println("You have left the " + this.getAddress());
+    }
+
+}
