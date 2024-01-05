a7542005a2249cb7cf13361a83e9897c1a1b4979
Rupa Dey <68509719+Rupa-Dey@users.noreply.github.com>
2024-01-05 18:53:36 +0600

    Update IStructure.java

--- HousingStructure/HousingStructure/IStructure.java
+++ HousingStructure/HousingStructure/IStructure.java
@@ -1,8 +1,11 @@
 package HousingStructure;
 
+//package CompositeDesignPattern;
+
 public interface IStructure {
     public void enter();
     public void exit();
     public void location();
-    public String getName();
+    public String getAddress();
 }
+
