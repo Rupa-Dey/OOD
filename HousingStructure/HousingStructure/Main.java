efd7b79da8842d6dfe113ece5b158c14978191b5
Rupa Dey <68509719+Rupa-Dey@users.noreply.github.com>
2024-01-05 18:52:24 +0600

    Update This pattern

--- HousingStructure/HousingStructure/Main.java
+++ HousingStructure/HousingStructure/Main.java
@@ -1,25 +1,30 @@
 package HousingStructure;
 
+//package CompositeDesignPattern;
+
 public class Main {
     public static void main(String[] args) {
-        Housing building = new Housing("123 Street");
-        Housing floor1 = new Housing("123 Street - First Floor");
-        int firstFloor = building.addStructure(floor1);
-        Room washroom1m = new Room("1F Men's Washroom");
-        Room washroom1w = new Room("1F Women's Washroom");
-        Room common1 = new Room("1F Common Area");
-        int firstMens = floor1.addStructure(washroom1m);
-        int firstCommon = floor1.addStructure(common1);
+        Housing building = new Housing("Uttora_302");
+        Housing floor  = new Housing("1st_floor");
+
+        int firstFloor = building.addStructure(floor);
+
+        Room washroom_m = new Room("1st Floor Men's washroom") ;
+        Room washroom_w = new Room("1st Floor Women's washroom");
+        Room common  = new Room("Common Washroom area");
+
+        int men_1F = floor.addStructure(washroom_m);
+        int women_1F  = floor.addStructure(washroom_w);
+        int commonBath = floor.addStructure(common);
+
         building.enter();
-        Housing currentFloor = (Housing)
-                building.getStructure(firstFloor);
+        Housing currentFloor = (Housing)building.getStructure(firstFloor);
         currentFloor.enter();
-        Room currentRoom = (Room)
-                currentFloor.getStructure(firstMens);
+        Room currentRoom = (Room)currentFloor.getStructure(men_1F);
         currentRoom.enter();
-        currentRoom = (Room)
-                currentFloor.getStructure(firstCommon);
-        currentRoom.enter();
-        building.exit();
+         currentRoom = (Room) currentFloor.getStructure(women_1F);
+         currentRoom.enter();
+
+         building.exit();
     }
 }
